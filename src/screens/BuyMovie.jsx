5ce39c4a9e9eec86ba74ee5d51bb392ee7a12834
Diniki <jayakodyjadn.19@uom.lk>
2022-07-25 16:36:04 +0530

    Buy movie, Movie cast pages added

---
+++ src/screens/BuyMovie.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function BuyMovie()
+{
+    return(
+        <>
+        <div style={{height:'100%', width:'100%', position: 'fixed', backgroundColor:'#040819'}}>
+  
+
+        </div>
+        </>
+    );
+}
+
+export default BuyMovie;
