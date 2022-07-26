d1cb523051936cddce44908d43c6e4ddb3117d3f
Diniki <jayakodyjadn.19@uom.lk>
2022-07-26 11:15:49 +0530

    Screen Layout - M

--- src/screens/common_components/ScreenLayout.jsx
+++ src/screens/common_components/ScreenLayout.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 const { Header, Content } = Layout;
 
 
-function Screen_Layout({department,searchText,searchBar,image,Component,bell,add,topSearchBar,home,back,addLink}){
+function Screen_Layout({department,image,Component,bell,add,topSearchBar,home,back,addLink}){
     const current = new Date();
     const navigate = useNavigate();
 
@@ -20,13 +20,13 @@ function Screen_Layout({department,searchText,searchBar,image,Component,bell,add
                 
                 <Layout>
                     <Header className={Screen_Layout_Styles.header}>
-                        {department==='Management' || department==='Administration'?<Topbar_Management department={department} img={image} bell={bell} add={add} topSearchBar={topSearchBar} home={home} back={back} addLink={addLink}/>
-                        :<Topbar department={department} home={home} back={back}/>}
+                        <Topbar department={department} home={home} back={back}/>
                       
                     </Header>
+
                     <Content className={Screen_Layout_Styles.content}>
                             <div className={Screen_Layout_Styles.subcontent}>
-                                {searchBar===1 ?<Title Text={searchText}/>:null }
+                               
                                 <div>
                                     {Component}
                                 </div>
