a837dee3eeac1de426bd2f9e325915ac4ba5cd58
Diniki <jayakodyjadn.19@uom.lk>
2022-07-29 22:29:00 +0530

    Home Page - layout

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import {Row, Col, Card, Space} from 'antd';
-import {HomeFilled} from '@ant-design/icons';
+import {LeftCircleOutlined,RightCircleOutlined} from '@ant-design/icons';
 import {Link} from 'react-router-dom';
 
-import SingleHomeStyles from "./SingleMovieHome.module.css"
+import HomePageStyles from "./HomePage.module.css"
 
 
 function SingleMovieHome()
 {
     return(
         <>
-        <h1>Hey</h1>
+        <div className={HomePageStyles.fullScreen}>
+            {/* Nav Bar */}
+            <div className={HomePageStyles.navBar}>
+            </div>
+
+            {/* Top Rated */}
+            <div className={HomePageStyles.topRated}>
+                <h2 style={{color:'#FFF504', textAlign:'left', paddingLeft:'20px', fontWeight:'600'}}>Top Rated</h2>
+                    <LeftCircleOutlined style={{fontSize:'52px', color:'white', float:'left', padding:'40px 0px 0px 10px', opacity:'0.8'}}/>
+                    <RightCircleOutlined style={{fontSize:'52px', color:'white', float:'right', padding:'40px 10px 0px 0px', opacity:'0.8'}}/>
+            </div>
+            {/* Recent Movies */}
+            <div className={HomePageStyles.recentMovies}>
+            <h2 style={{color:'#FFF504', textAlign:'left', paddingLeft:'20px', fontWeight:'600'}}>Recent</h2>
+
+                {/* Footer */}
+                <div className={HomePageStyles.footer}></div>
+            </div>
+        </div>
         </>
     );
 }
