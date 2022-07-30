1ebda4da03a022b1c61671e89096a201d648e8cc
Diniki <jayakodyjadn.19@uom.lk>
2022-07-30 10:36:08 +0530

    top rated movie card

---
+++ src/screens/TopRatedMovieCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TopRatedMovieCard ()
+{
+    return(
+        <>
+        <div style={{height:'160px', width:'140px',backgroundColor:'white', borderRadius:'15px'}}>
+
+        </div>
+        </>
+    );
+}
+
+export default TopRatedMovieCard;
