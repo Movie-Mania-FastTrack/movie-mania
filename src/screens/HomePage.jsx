c37acc27015190409177f9513a83b6bf5fce2e47
Diniki <jayakodyjadn.19@uom.lk>
2022-07-30 10:35:46 +0530

    HomePage - M

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -3,6 +3,9 @@ import {Row, Col, Card, Space} from 'antd';
 import {LeftCircleOutlined,RightCircleOutlined} from '@ant-design/icons';
 import {Link} from 'react-router-dom';
 
+import TopRatedMovieCard from './TopRatedMovieCard';
+import RecentMovieCard from './RecentMovieCard';
+
 import HomePageStyles from "./HomePage.module.css"
 
 
@@ -18,13 +21,24 @@ function SingleMovieHome()
             {/* Top Rated */}
             <div className={HomePageStyles.topRated}>
                 <h2 style={{color:'#FFF504', textAlign:'left', paddingLeft:'20px', fontWeight:'600'}}>Top Rated</h2>
-                    <LeftCircleOutlined style={{fontSize:'52px', color:'white', float:'left', padding:'40px 0px 0px 10px', opacity:'0.8'}}/>
-                    <RightCircleOutlined style={{fontSize:'52px', color:'white', float:'right', padding:'40px 10px 0px 0px', opacity:'0.8'}}/>
+                <Row>
+                    <Col span={2}>
+                        <LeftCircleOutlined style={{fontSize:'52px', color:'white', float:'left', padding:'40px 0px 0px 10px', opacity:'0.8'}}/>
+                    </Col>
+                    <Col span={20}><TopRatedMovieCard/></Col>
+                    <Col span={2}>
+                        <RightCircleOutlined style={{fontSize:'52px', color:'white', float:'right', padding:'40px 10px 0px 0px', opacity:'0.8'}}/>
+                    </Col>
+                </Row>
             </div>
             {/* Recent Movies */}
             <div className={HomePageStyles.recentMovies}>
             <h2 style={{color:'#FFF504', textAlign:'left', paddingLeft:'20px', fontWeight:'600'}}>Recent</h2>
-
+                <Row>
+                    <Col span={1}></Col>
+                    <Col span={22}><RecentMovieCard/></Col>
+                    <Col span={1}></Col>
+                </Row>
                 {/* Footer */}
                 <div className={HomePageStyles.footer}></div>
             </div>
