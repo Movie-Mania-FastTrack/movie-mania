e0bb3d18de048f5899575ad0f592166a29d5fccc
Diniki <jayakodyjadn.19@uom.lk>
2022-08-01 12:22:45 +0530

    Admin Home page - movie request page

--- src/screens/AdminHomePageLayout.jsx
+++ src/screens/AdminHomePageLayout.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-
-import JaggaJasoos from '../resources/videos/JaggaJasoos.mp4';
+import MovieRequest  from "./MovieRequest";
 
 
 function AdminHomePageLayout()
@@ -8,8 +7,8 @@ function AdminHomePageLayout()
     return(
         <>
         <div style={{height:'100%', width:'100%', position: 'fixed', backgroundColor:'#040819'}}>
-          
-            </div>
+            <MovieRequest/>
+        </div>
 
         </>
     );
