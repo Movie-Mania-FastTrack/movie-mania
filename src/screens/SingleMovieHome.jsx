ed4e0eaab654c53ca9cef247e2fc8b7b369ccf93
Diniki <jayakodyjadn.19@uom.lk>
2022-08-08 13:22:03 +0530

    single home design - M

--- src/screens/SingleMovieHome.jsx
+++ src/screens/SingleMovieHome.jsx
@@ -19,14 +19,14 @@ function SingleMovieHome()
 
                 {/* Home button */}
                     <div
-                        style={{position:'fixed', height:'80px', width:'80px', 
+                        style={{height:'80px', width:'80px', 
                         borderRadius:'50%', backgroundColor:'#FBFAF6', opacity:'0.3',
                         margin:'20px 20px 0px 0px'}}>
                             <HomeFilled style={{fontSize:'45px', margin:'17px 0px 0px 17px'}}/>
                     </div>
 
                        {/* Single movie Options */}
-                    <div style={{paddingTop:'180px', position:'fixed'}}>
+                    <div style={{paddingTop:'180px'}}>
                             <Space direction='vertical'>
                                 <Link to='/movie_trailor_page'>
                                     <Card style={{height:'60px', width:'200px',backgroundColor:'#FBFAF6',opacity:'0.4', borderRadius:'12px'}}> 
